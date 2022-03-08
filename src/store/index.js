24747540909475522185d0c6e3307032ae4bc722
Selecta <odilzhon100@gmail.com>
2022-03-08 17:09:34 +0500

    Add action for get cards

--- src/store/index.js
+++ src/store/index.js
@@ -1,35 +1,12 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-// import Cards from "./modules/cards";
+import Cards from "./modules/cards";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state:{
-    cards: [],
-  },
-  actions: {
-    async getCards({ commit }) {
-      console.log("getcars");
-      await http
-        .get("/cards")
-        .then((res) => {
-          const cards = res;
-          commit("SUCCESS_GET_CARDS", cards);
-        })
-        .catch((err) => {
-          commit("ERROR_GET_CARDS", err);
-        });
-    },
-  },
-  mutations: {
-    SUCCESS_GET_CARDS(state, pyaload) {
-      console.log(pyaload);
-      state.cards = pyaload;
-    },
-    ERROR_GET_CARDS(state, err) {
-      console.log(err);
-    },
+  modules: {
+    Cards,
   },
 });
